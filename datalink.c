65c5947ecd69afa659d52859389c1ba6c8064680
agicy <2359800311@qq.com>
2024-04-04 19:19:23 +0800

    Standardized code by incorporating stdint.h

--- datalink.c
+++ datalink.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 #include "protocol.h"
 #include "datalink.h"
 
 #define DATA_TIMER  2000
 
-struct FRAME { 
-    unsigned char kind; /* FRAME_DATA */
-    unsigned char ack;
-    unsigned char seq;
-    unsigned char data[PKT_LEN]; 
-    unsigned int  padding;
+struct FRAME {
+    uint8_t kind; /* FRAME_DATA */
+    uint8_t ack;
+    uint8_t seq;
+    uint8_t data[PKT_LEN];
+    uint32_t  padding;
 };
 
-static unsigned char frame_nr = 0, buffer[PKT_LEN], nbuffered;
-static unsigned char frame_expected = 0;
+static uint8_t frame_nr = 0, buffer[PKT_LEN], nbuffered;
+static uint8_t frame_expected = 0;
 static int phl_ready = 0;
 
-static void put_frame(unsigned char *frame, int len)
+static void put_frame(uint8_t *frame, int len)
 {
-    *(unsigned int *)(frame + len) = crc32(frame, len);
+    *(uint32_t *)(frame + len) = crc32(frame, len);
     send_frame(frame, len + 4);
     phl_ready = 0;
 }
@@ -36,7 +37,7 @@ static void send_data_frame(void)
 
     dbg_frame("Send DATA %d %d, ID %d\n", s.seq, s.ack, *(short *)s.data);
 
-    put_frame((unsigned char *)&s, 3 + PKT_LEN);
+    put_frame((uint8_t *)&s, 3 + PKT_LEN);
     start_timer(frame_nr, DATA_TIMER);
 }
 
@@ -49,7 +50,7 @@ static void send_ack_frame(void)
 
     dbg_frame("Send ACK  %d\n", s.ack);
 
-    put_frame((unsigned char *)&s, 2);
+    put_frame((uint8_t *)&s, 2);
 }
 
 int main(int argc, char **argv)
@@ -58,8 +59,8 @@ int main(int argc, char **argv)
     struct FRAME f;
     int len = 0;
 
-    protocol_init(argc, argv); 
-    lprintf("Designed by Jiang Yanjun, build: " __DATE__"  "__TIME__"\n");
+    protocol_init(argc, argv);
+    lprintf("Designed by Lu Anlai, build: " __DATE__"  "__TIME__"\n");
 
     disable_network_layer();
 
@@ -77,13 +78,13 @@ int main(int argc, char **argv)
             phl_ready = 1;
             break;
 
-        case FRAME_RECEIVED: 
-            len = recv_frame((unsigned char *)&f, sizeof f);
-            if (len < 5 || crc32((unsigned char *)&f, len) != 0) {
+        case FRAME_RECEIVED:
+            len = recv_frame((uint8_t *)&f, sizeof f);
+            if (len < 5 || crc32((uint8_t *)&f, len) != 0) {
                 dbg_event("**** Receiver Error, Bad CRC Checksum\n");
                 break;
             }
-            if (f.kind == FRAME_ACK) 
+            if (f.kind == FRAME_ACK)
                 dbg_frame("Recv ACK  %d\n", f.ack);
             if (f.kind == FRAME_DATA) {
                 dbg_frame("Recv DATA %d %d, ID %d\n", f.seq, f.ack, *(short *)f.data);
@@ -92,16 +93,16 @@ int main(int argc, char **argv)
                     frame_expected = 1 - frame_expected;
                 }
                 send_ack_frame();
-            } 
+            }
             if (f.ack == frame_nr) {
                 stop_timer(frame_nr);
                 nbuffered--;
                 frame_nr = 1 - frame_nr;
             }
-            break; 
+            break;
 
         case DATA_TIMEOUT:
-            dbg_event("---- DATA %d timeout\n", arg); 
+            dbg_event("---- DATA %d timeout\n", arg);
             send_data_frame();
             break;
         }
